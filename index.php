601589874ed8e3d4a7c7ada2ed42edfe20b41867
TheMainx <120021243+TheMainx@users.noreply.github.com>
2022-12-07 12:23:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penetration tests</title>
+    <link rel="stylesheet" href="style.css" />   
+</head>
+<body>
+    <h1>This website is handed over for hacking!</h1>
+    <h2>For your safety, please leave this website.</h2>
+    <?php
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    $date = date('d-m-Y, H:i:s');
+    $plik = file_get_contents('IpAndDates.txt', "w");
+    fwrite($plik, "IP: "{$ip} " Date: "{$date});
+    fclose($plik);
+    ?>
+</body>
+</html>

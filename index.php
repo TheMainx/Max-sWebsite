fa13497b72ef3d42f93dc4e72a30334d066ee635
TheMainx <120021243+TheMainx@users.noreply.github.com>
2022-12-07 14:04:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Penetration tests</title>
-    <link rel="stylesheet" href="style.css" />   
-</head>
-<body>
-    <h1>This website is handed over for hacking!</h1>
-    <h2>For your safety, please leave this website.</h2>
-    
-</body>
-</html>
+
